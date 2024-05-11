8c0de975b9c726dbe4ecedbcac3eef39fe69286b
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2024-05-11 22:10:01 +0530

    dev: angular json updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavbarComponent, ContactComponent],
+  imports: [RouterOutlet, FooterComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
