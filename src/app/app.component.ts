d26eb47ddb8d5495de44db2c8ad5200895634177
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2024-05-12 15:06:48 +0530

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { VideoComponent } from './components/video/video.component';
+import { AboutComponent } from './components/about/about.component';
+import { InfoComponent } from './components/info/info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, ContactComponent],
+  imports: [RouterOutlet,VideoComponent, HeroComponent, AboutComponent, InfoComponent, ContactComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
