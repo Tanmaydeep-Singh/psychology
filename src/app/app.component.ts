70ceda9d1ddf377482ba6f54d37ea8fb7f76418a
Harpreet <harpeet.tech.vin@gmail.com>
2024-05-23 20:07:07 +0530

    dev: second updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeroComponent } from './components/hero/hero.component';
 import { VideoComponent } from './components/video/video.component';
 import { AboutComponent } from './components/about/about.component';
 import { InfoComponent } from './components/info/info.component';
+import { QualificationsComponent } from './components/qualifications/qualifications.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,VideoComponent, HeroComponent, AboutComponent, InfoComponent, ContactComponent],
+  imports: [RouterOutlet,VideoComponent, HeroComponent, AboutComponent, InfoComponent, QualificationsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
